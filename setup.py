b368cc10e905df41ea54886ac83b6d9c40b1373e
badrbin <124630341+badrbin@users.noreply.github.com>
2025-07-14 01:54:33 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='namar_custom',
+    version='0.1.0',
+    description='App مخصص لكشف حساب أوامر البيع والدفعات',
+    author='Your Name',
+    packages=find_packages(),
+    install_requires=['frappe'],
+)
