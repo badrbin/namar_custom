99212654408a68d1bbca837fd62a149f55b48a27
badrbin <124630341+badrbin@users.noreply.github.com>
2025-07-14 01:58:25 +0300

    Add files via upload

---
+++ namar_custom/hooks.py
@@ -0,0 +1,12 @@
+
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+app_name = "namar_custom"
+app_title = "Namar Custom"
+app_publisher = "Your Name"
+app_description = "App مخصص لكشف حساب أوامر البيع والدفعات"
+app_icon = "octicon octicon-file-directory"
+app_color = "grey"
+app_email = "you@example.com"
+app_license = "MIT"
