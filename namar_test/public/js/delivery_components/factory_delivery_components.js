a4c252bbd0a04f6a43b1f8727c82df87d78ac8be
badrbin <124630341+badrbin@users.noreply.github.com>
2026-08-08 06:53:02 +0300

    أكمل تعريف واجهات مسح الحزم

--- namar_test/public/js/delivery_components/factory_delivery_components.js
+++ namar_test/public/js/delivery_components/factory_delivery_components.js
@@ -1,5 +1,5 @@
 (function() {
-  window.NAMAR_DELIVERY_COMPONENT_API = window.NAMAR_DELIVERY_COMPONENT_API || {
+  var defaults = {
     sync: "namar_test.delivery_components.api.sync_delivery_component_packages",
     get: "namar_test.delivery_components.api.get_delivery_component_packages",
     markReady: "namar_test.delivery_components.api.mark_delivery_component_package_ready",
@@ -7,4 +7,9 @@
     fulfillment: "namar_test.delivery_components.api.get_material_request_fulfillment_readiness",
     resolve: "namar_test.delivery_components.api.resolve_delivery_tracking_code"
   };
+  window.NAMAR_DELIVERY_COMPONENT_API = Object.assign(
+    {},
+    defaults,
+    window.NAMAR_DELIVERY_COMPONENT_API || {}
+  );
 })();
